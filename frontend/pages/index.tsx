4a138e8e199b3d208b075fce06a353b772456d76
S Sitaraman <sitaraman.raghav.india@gmail.com>
2022-12-02 21:08:36 +0530

    Init

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,66 @@
+import { useWeb3React } from "@web3-react/core";
+import Head from "next/head";
+import Link from "next/link";
+import Account from "../components/Account";
+import ETHBalance from "../components/ETHBalance";
+import TokenBalance from "../components/TokenBalance";
+import useEagerConnect from "../hooks/useEagerConnect";
+
+const DAI_TOKEN_ADDRESS = "0x6b175474e89094c44da98b954eedeac495271d0f";
+
+function Home() {
+  const { account, library } = useWeb3React();
+
+  const triedToEagerConnect = useEagerConnect();
+
+  const isConnected = typeof account === "string" && !!library;
+
+  return (
+    <div>
+      <Head>
+        <title>next-web3-boilerplate</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+        <nav>
+          <Link href="/">
+            <a>next-web3-boilerplate</a>
+          </Link>
+
+          <Account triedToEagerConnect={triedToEagerConnect} />
+        </nav>
+      </header>
+
+      <main>
+        <h1>
+          Welcome to{" "}
+          <a href="https://github.com/mirshko/next-web3-boilerplate">
+            next-web3-boilerplate
+          </a>
+        </h1>
+
+        {isConnected && (
+          <section>
+            <ETHBalance />
+
+            <TokenBalance tokenAddress={DAI_TOKEN_ADDRESS} symbol="DAI" />
+          </section>
+        )}
+      </main>
+
+      <style jsx>{`
+        nav {
+          display: flex;
+          justify-content: space-between;
+        }
+
+        main {
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default Home;
