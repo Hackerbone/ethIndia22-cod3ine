7a0f4e66f9640c5ebbcaf5c896df027ff0e53e8e
kathan3009 <kthndesai@gmail.com>
2022-12-03 16:50:15 +0530

    changes

--- frontend/contracts/Squad.sol
+++ frontend/contracts/Squad.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+// PUSH Comm Contract Interface
+interface IPUSHCommInterface {
+    function sendNotification(
+        address _channel,
+        address _recipient,
+        bytes calldata _identity
+    ) external;
+}
+
 contract Squad {
     address admin;
     string orgName;
@@ -29,9 +38,17 @@ contract Squad {
         orgName = _orgName;
     }
 
-    function getOrgDetails () public view returns (string memory, address, Employee[] memory) {
+    function getOrgDetails()
+        public
+        view
+        returns (
+            string memory,
+            address,
+            Employee[] memory
+        )
+    {
         Employee[] memory employees = new Employee[](employeeAddrs.length);
-        for (uint i = 0; i < employeeAddrs.length; i++) {
+        for (uint256 i = 0; i < employeeAddrs.length; i++) {
             employees[i] = Employee(
                 employeeNames[employeeAddrs[i]],
                 employeeAddrs[i]
@@ -41,10 +58,28 @@ contract Squad {
     }
 
     // function to add employees to the organization
-    function addEmployee(
-        address _employeeAddress,
-        string memory _employeeName
-    ) public {
+    function addEmployee(address _employeeAddress, string memory _employeeName)
+        public
+    {
+        IPUSHCommInterface(0xb3971BCef2D791bc4027BbfedFb47319A4AAaaAa)
+            .sendNotification(
+                0x3d6e6678E43ecd302867EE0c92bcBF2Fd6C60239, // from channel - recommended to set channel via dApp and put it's value -> then once contract is deployed, go back and add the contract address as delegate for your channel
+                _employeeAddress, // to recipient, put address(this) in case you want Broadcast or Subset. For Targetted put the address to which you want to send
+                bytes(
+                    string(
+                        // We are passing identity here: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
+                        abi.encodePacked(
+                            "0", // this is notification identity: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
+                            "+", // segregator
+                            "3", // this is payload type: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/payload (1, 3 or 4) = (Broadcast, targetted or subset)
+                            "+", // segregator
+                            "Title", // this is notificaiton title
+                            "+", // segregator
+                            "Body" // notification body
+                        )
+                    )
+                )
+            );
         require(msg.sender == admin, "Only admin can add employees");
         require(
             bytes(_employeeName).length > 0,
@@ -84,7 +119,7 @@ contract Squad {
     ) public {
         require(msg.sender == admin, "Only admin can add employees to groups");
         bool employeeExists = false;
-        for (uint i = 0; i < employeeAddrs.length; i++) {
+        for (uint256 i = 0; i < employeeAddrs.length; i++) {
             if (employeeAddrs[i] == _employeeAddress) {
                 employeeExists = true;
                 break;
@@ -97,7 +132,7 @@ contract Squad {
         );
         // check if employee is already in the group
         Group storage group = groupMap[_groupName];
-        for (uint i = 0; i < group.members.length; i++) {
+        for (uint256 i = 0; i < group.members.length; i++) {
             if (group.members[i] == _employeeAddress) {
                 revert("Employee already exists in the group");
             }
@@ -123,7 +158,7 @@ contract Squad {
             "Employee does not exist"
         );
         Group storage group = groupMap[_groupName];
-        for (uint i = 0; i < group.members.length; i++) {
+        for (uint256 i = 0; i < group.members.length; i++) {
             if (group.members[i] == _employeeAddress) {
                 group.members[i] = group.members[group.members.length - 1];
                 group.members.pop();
@@ -140,7 +175,7 @@ contract Squad {
     ) public {
         Group storage group = groupMap[_groupName];
         bool memberExists = false;
-        for (uint i = 0; i < group.members.length; i++) {
+        for (uint256 i = 0; i < group.members.length; i++) {
             if (group.members[i] == msg.sender) {
                 memberExists = true;
                 break;
@@ -162,10 +197,9 @@ contract Squad {
     }
 
     // delete file from group, only allowed by admin or other members in group
-    function deleteFileFromGroup(
-        string memory _groupName,
-        address _fileAddress
-    ) public {
+    function deleteFileFromGroup(string memory _groupName, address _fileAddress)
+        public
+    {
         require(_fileAddress != address(0), "File address is required");
         require(
             bytes(groupMap[_groupName].name).length > 0,
@@ -174,7 +208,7 @@ contract Squad {
 
         Group storage group = groupMap[_groupName];
         bool memberExists = false;
-        for (uint i = 0; i < group.members.length; i++) {
+        for (uint256 i = 0; i < group.members.length; i++) {
             if (group.members[i] == msg.sender) {
                 memberExists = true;
                 break;
@@ -185,7 +219,7 @@ contract Squad {
             "Only admin or members of group can delete files from group"
         );
 
-        for (uint i = 0; i < group.files.length; i++) {
+        for (uint256 i = 0; i < group.files.length; i++) {
             if (group.files[i] == _fileAddress) {
                 group.files[i] = group.files[group.files.length - 1];
                 group.files.pop();
@@ -195,16 +229,18 @@ contract Squad {
     }
 
     // get all files with their filenames in a group
-    function getFilesInGroup(
-        string memory _groupName
-    ) public view returns (address[] memory, string[] memory) {
+    function getFilesInGroup(string memory _groupName)
+        public
+        view
+        returns (address[] memory, string[] memory)
+    {
         require(
             bytes(groupMap[_groupName].name).length > 0,
             "Group does not exist"
         );
         Group storage group = groupMap[_groupName];
         string[] memory fileNamesInGroup = new string[](group.files.length);
-        for (uint i = 0; i < group.files.length; i++) {
+        for (uint256 i = 0; i < group.files.length; i++) {
             fileNamesInGroup[i] = fileNames[group.files[i]];
         }
         return (group.files, fileNamesInGroup);
@@ -218,7 +254,7 @@ contract Squad {
     // get all employees with their names as employee array
     function getEmployees() public view returns (Employee[] memory) {
         Employee[] memory employees = new Employee[](employeeAddrs.length);
-        for (uint i = 0; i < employeeAddrs.length; i++) {
+        for (uint256 i = 0; i < employeeAddrs.length; i++) {
             employees[i] = Employee(
                 employeeNames[employeeAddrs[i]],
                 employeeAddrs[i]
@@ -228,9 +264,11 @@ contract Squad {
     }
 
     // get all employees in a group with their name as one object
-    function getEmployeesInGroup(
-        string memory _groupName
-    ) public view returns (Employee[] memory) {
+    function getEmployeesInGroup(string memory _groupName)
+        public
+        view
+        returns (Employee[] memory)
+    {
         // check if group exists
         require(
             bytes(groupMap[_groupName].name).length > 0,
@@ -238,7 +276,7 @@ contract Squad {
         );
         Group storage group = groupMap[_groupName];
         Employee[] memory employees = new Employee[](group.members.length);
-        for (uint i = 0; i < group.members.length; i++) {
+        for (uint256 i = 0; i < group.members.length; i++) {
             employees[i] = Employee(
                 employeeNames[group.members[i]],
                 group.members[i]
@@ -249,8 +287,8 @@ contract Squad {
 
     // function to check if user in employee list
     function isEmployee(address _employeeAddress) public view returns (bool) {
-        if(_employeeAddress == admin) return true;
-        for (uint i = 0; i < employeeAddrs.length; i++) {
+        if (_employeeAddress == admin) return true;
+        for (uint256 i = 0; i < employeeAddrs.length; i++) {
             if (employeeAddrs[i] == _employeeAddress) {
                 return true;
             }
