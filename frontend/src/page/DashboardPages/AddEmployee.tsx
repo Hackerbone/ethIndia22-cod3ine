140bcdcef4c442bb48a6ad4c4d690ecd992575f3
S Sitaraman <sitaraman.raghav.india@gmail.com>
2022-12-03 12:38:15 +0530

    Add employee

--- frontend/src/page/DashboardPages/AddEmployee.tsx
+++ frontend/src/page/DashboardPages/AddEmployee.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 
-import {
-  Button,
-  Form,
-  Input,
-  //  Upload
-} from "antd";
+import { Button, Form, Input } from "antd";
 import { addEmployee } from "../../services/services";
 // import { useNavigate } from "react-router-dom";
 
@@ -29,12 +24,6 @@ function AddEmployee() {
       <Form.Item name="employeeName">
         <Input placeholder="employeename" />
       </Form.Item>
-
-      {/* <Form.Item name="file">
-        <Upload>
-          <Button>Upload File</Button>
-        </Upload>
-      </Form.Item> */}
 
       <Button htmlType="submit">Submit</Button>
     </Form>
