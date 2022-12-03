683adaa0da1f15ca497a38539350561fdd09f051
kathan3009 <kthndesai@gmail.com>
2022-12-03 17:44:21 +0530

    integrations - org, gropus

--- frontend/src/page/Organisations/Organisations.tsx
+++ frontend/src/page/Organisations/Organisations.tsx
@@ -1,60 +1,79 @@
-import { useState } from 'react';
-import { FaEthereum } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
-import TableComponent from '../../components/common/TableComponent';
-import DashboardLayout from '../../components/DashboardLayout/DashboardLayout'
-
-
+import { useState, useEffect } from "react";
+import { FaEthereum } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import TableComponent from "../../components/common/TableComponent";
+import DashboardLayout from "../../components/DashboardLayout/DashboardLayout";
+import { getOrganizationDetails } from "../../services/services";
 
 const Organisations = () => {
-    const navigate = useNavigate()
-    const [data, setData] = useState([
-        {
-            key: "1",
-            orgName: "Bugbase Organisations",
-            address: "1231239182798127",
-            groups: 3,
-            role: "Owner"
-        }
-    ])
+  const navigate = useNavigate();
+  const [data, setData] = useState([
+    {
+      key: "1",
+      orgName: "Bugbase Organisations",
+      address: "1231239182798127",
+      employees: 3,
+      role: "Owner",
+    },
+  ]);
 
-    const columns = [
+  const columns = [
+    {
+      title: "Organisation Name",
+      dataIndex: "orgName",
+      key: "orgName",
+      render: (orgName: any) => (
+        <div className="orgtable-orgName" onClick={() => navigate("/groups")}>
+          {orgName}
+        </div>
+      ),
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+      render: (add: any) => (
+        <div style={{ cursor: "pointer" }}>
+          <FaEthereum style={{ marginRight: 6 }} />
+          {add}
+        </div>
+      ),
+    },
+    {
+      title: "Groups",
+      dataIndex: "groups",
+      key: "groups",
+      render: (groups: any) => <div>{groups} Groups</div>,
+    },
+    {
+      title: "Role",
+      dataIndex: "role",
+      key: "role",
+    },
+  ];
+  // use effect call getOrganisationDetails
+  useEffect(() => {
+    const getOrganisationDetails = async () => {
+      const res = await getOrganizationDetails();
+      console.log("Organisation details", res);
+      setData([
         {
-            title: 'Organisation Name',
-            dataIndex: 'orgName',
-            key: 'orgName',
-            render: (orgName: any) =>
-                <div className="orgtable-orgName" onClick={() => navigate("/groups")}>{orgName}</div>
-    
+          key: "1",
+          orgName: res[0],
+          address: res[1],
+          employees: res[2].length,
+          role: "Owner",
         },
-        {
-            title: 'Address',
-            dataIndex: 'address',
-            key: 'address',
-            render: (add: any) =>
-                <div style={{ cursor: "pointer" }}><FaEthereum style={{ marginRight: 6 }} />{add}</div>
-        },
-        {
-            title: 'Groups',
-            dataIndex: 'groups',
-            key: 'groups',
-            render: (groups: any) =>
-                <div>{groups} Groups</div>
-    
-        },
-        {
-            title: 'Role',
-            dataIndex: 'role',
-            key: 'role',
-        },
-    ];
-    
+      ]);
+    };
+    getOrganisationDetails();
+  }, []);
 
-    return (
-        <DashboardLayout title={"Organisations"}>
-            <TableComponent columns={columns} dataSource={data} />
-        </DashboardLayout>
-    )
-}
+  return (
+    <DashboardLayout title={"Organisations"}>
+      <TableComponent columns={columns} dataSource={data} />
+    </DashboardLayout>
+  );
+};
 
-export default Organisations
+export default Organisations;
