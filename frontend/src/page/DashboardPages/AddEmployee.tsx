a51f6c0e188cf3084cfc56f2ae769f1cdbe0cc29
S Sitaraman <sitaraman.raghav.india@gmail.com>
2022-12-03 14:30:59 +0530

    Add employee

--- frontend/src/page/DashboardPages/AddEmployee.tsx
+++ frontend/src/page/DashboardPages/AddEmployee.tsx
@@ -1,15 +1,55 @@
 import React from "react";
-
-import { Button, Form, Input, Upload } from "antd";
-import { handleFileUpload } from "../../services/fileUpload";
-// import { useNavigate } from "react-router-dom";
+import { Button, Form, Input } from "antd";
+import { addEmployee } from "../../services/services";
 
 function AddEmployee() {
   return (
-    <form onSubmit={async (data) => {}}>
-      <input name="file" type="file" />
-      <button type="submit">Upload File</button>
-    </form>
+    <Form
+      onFinish={async (values) => {
+        console.log(values);
+        const res = await addEmployee(
+          values.employeeWalletAddress,
+          values.employeeName
+        );
+
+        console.log("Add employee res", res);
+      }}
+      layout="vertical"
+    >
+      <h1 className="heading">Add an employee to your Organization</h1>
+      <p>
+        You just require the employee's wallet address and name to add them to
+        your organization.
+      </p>
+      <br />
+      <Form.Item
+        label="Employee Name"
+        name="employeeName"
+        rules={[
+          {
+            required: true,
+            message: "Please input your employee name!",
+          },
+        ]}
+      >
+        <Input placeholder="Someone Joe" />
+      </Form.Item>
+      <Form.Item
+        label="Employee Wallet Address"
+        name="employeeWalletAddress"
+        rules={[
+          {
+            required: true,
+            message: "Please input your employee EOC ID!",
+          },
+        ]}
+      >
+        <Input placeholder="0x..." />
+      </Form.Item>
+      <Button type="primary" htmlType="submit">
+        Add Employee to Organization
+      </Button>
+    </Form>
   );
 }
 
